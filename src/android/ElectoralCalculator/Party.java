23e3e75f994981d73b938db153cb23e030b45361
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2012-08-10 18:22:10 +0200

    Adapted the Party class to include votePercent and seats

--- src/android/ElectoralCalculator/Party.java
+++ src/android/ElectoralCalculator/Party.java
@@ -3,12 +3,21 @@ package android.ElectoralCalculator;
 public class Party {
 	private String name;
 	private int votes;
+	private double votePercent;
+	private int seats;
 	
-	// Constructor for the Party class
-	public Party(String name, int votes) {
+	// General constructor for the Party class
+	public Party(String name, int votes, double votePercent, int seats) {
 		super();
 		this.name = name;
 		this.votes = votes;
+		this.votePercent = votePercent;
+		this.seats = seats;
+	}
+	
+	// Constructor for the Party class with name and votes parameters
+	public Party(String name, int votes) {
+		this(name, votes, 0.0, 0);
 	}
 	
 	// Getter and setter methods for all the fields.
@@ -23,5 +32,17 @@ public class Party {
 	}
 	public void setVotes(int votes) {
 		this.votes = votes;
+	}
+	public double getVotePercent(String name) {
+		return this.votePercent;
+	}
+	public void setVotePercent(double votePercent) {
+		this.votePercent = votePercent;
+	}
+	public int getSeats() {
+		return this.seats;
+	}
+	public void setSeats(int seats) {
+		this.seats = seats;
 	}
 }
