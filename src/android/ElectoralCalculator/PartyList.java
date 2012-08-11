777a358b2f46358cf999effcced407622731d11f
Asier Iturralde Sarasola <asier.iturralde@gmail.com>
2012-08-11 16:18:49 +0200

    Changed PartyList to extend ArrayList<Party> as I need the inherited methods

--- src/android/ElectoralCalculator/PartyList.java
+++ src/android/ElectoralCalculator/PartyList.java
@@ -2,14 +2,16 @@ package android.ElectoralCalculator;
 
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.List;
 
-public class PartyList {
-
-    private ArrayList<Party> partyList;
-
-    public PartyList(){
-        partyList = new ArrayList<Party>();
-    }
+public class PartyList extends ArrayList<Party> {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8207224441259052084L;
+	
+	private List<Party> partyList = new ArrayList<Party>();
 
     public void getPartyName(int position){
          partyList.get(position).getName().toString();
